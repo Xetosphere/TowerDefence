30829e9cf30248bb3595f3047930b1ecc9721b0c
Xetosphere <stabtechgames@gmail.com>
2015-10-12 22:48:43 +0200

    Added some stuff to the AI

---
+++ TowerDefence/Assets/Scripts/MinionScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum AttackType
+{
+    Water, Fire, Rock, Lightning, Ice, Wind
+};
+
+public class MinionScript : MonoBehaviour {
+
+    // Public
+
+    public int health = 50;
+    public int damage = 1;
+    public AttackType weakness = AttackType.Fire;
+    public AttackType resistance = AttackType.Water;
+
+    // Private
+
+
+    void Start()
+    {
+        Debug.Log("Name: " + transform.name + ", Health: " + health + ", Damage: " + damage + ", Weakness: " + weakness + ", Resistance: " + resistance);
+    }
+}
--- TowerDefence/Assets/Scripts/WaypointAI.cs
+++ TowerDefence/Assets/Scripts/WaypointAI.cs
@@ -1,6 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum Direction
+{
+    Up, Right, Down, Left
+};
+
 public class WaypointAI : MonoBehaviour {
 
     // Public
@@ -8,23 +13,36 @@ public class WaypointAI : MonoBehaviour {
     public GameObject[] waypoints;
 
     public float moveSpeed = 5.0f;
-    public float rotationSpeed = 5.0F;
+    public float rotationSpeed = 5.0f;
+    public float attackSpeed = 1.0f;
 
     public string waypointTag = "Waypoint";
 
+    public Direction defaultDirection = Direction.Down;
+
     // Private
-    int currentWaypoint = 0;
     Transform targetWaypoint;
+    Animator anim;
+
+    bool isMoving = false;
+
+    int currentWaypoint = 0;
+
+    Direction dir;
 
     void Start()
     {
+        anim = transform.GetComponent<Animator>();
+
         waypoints = GameObject.FindGameObjectsWithTag(waypointTag);
+        dir = defaultDirection;
     }
 
     void Update()
     {
         if (currentWaypoint < waypoints.Length)
         {
+            isMoving = true;
             for (int i = 0; i < waypoints.Length; i++)
             {
                 WaypointScript script = waypoints[i].GetComponent<WaypointScript>();
@@ -37,19 +55,33 @@ public class WaypointAI : MonoBehaviour {
 
             if (targetWaypoint != null)
             {
+                if (transform.position.y < targetWaypoint.position.y)
+                    dir = Direction.Up;
+                else if (transform.position.y > targetWaypoint.position.y)
+                    dir = Direction.Down;
+                else if (transform.position.x < targetWaypoint.position.x)
+                    dir = Direction.Right;
+                else if (transform.position.x > targetWaypoint.position.x)
+                    dir = Direction.Left;
 
                 Move(targetWaypoint.position);
 
                 float distance = Vector2.Distance(transform.position, targetWaypoint.position);
                 
-                if (distance < 0.001f)
+                if (distance <= 0f)
                     currentWaypoint++;
             }
         }
         else
         {
-            GameObject.Destroy(this.gameObject);
+            isMoving = false;
         }
+    }
+
+    void ApplyCorrectAnimation(Direction direction)
+    {
+        anim.SetInteger("Direction", (int)direction);
+        anim.SetBool("isMoving", isMoving);
     }
 
     void Move(Vector2 target)
