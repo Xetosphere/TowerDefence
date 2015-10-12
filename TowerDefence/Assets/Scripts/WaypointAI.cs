a5398ed42da1f17cba21fa3e81bfaec9b631af5b
Xetosphere <mattistabell@gmail.com>
2015-10-12 02:32:12 +0200

    Added simple WaypointAI
    Added the simple_enemy which walks in a path using the WaypointAI script
    with the Waypoint prefab. Moves from one Waypoint prefab to the next.

---
+++ TowerDefence/Assets/Scripts/WaypointAI.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaypointAI : MonoBehaviour {
+
+    // Public
+
+    public GameObject[] waypoints;
+
+    public float moveSpeed = 5.0f;
+    public float rotationSpeed = 5.0F;
+
+    public string waypointTag = "Waypoint";
+
+    // Private
+    int currentWaypoint = 0;
+    Transform targetWaypoint;
+
+    void Start()
+    {
+        waypoints = GameObject.FindGameObjectsWithTag(waypointTag);
+    }
+
+    void Update()
+    {
+        if (currentWaypoint < waypoints.Length)
+        {
+            for (int i = 0; i < waypoints.Length; i++)
+            {
+                WaypointScript script = waypoints[i].GetComponent<WaypointScript>();
+
+                if (script.queuePosition == currentWaypoint)
+                {
+                    targetWaypoint = waypoints[i].transform;
+                }
+            }
+
+            if (targetWaypoint != null)
+            {
+
+                Move(targetWaypoint.position);
+
+                float distance = Vector2.Distance(transform.position, targetWaypoint.position);
+                
+                if (distance < 0.001f)
+                    currentWaypoint++;
+            }
+        }
+        else
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+
+    void Move(Vector2 target)
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+    }
+}
---
+++ TowerDefence/Assets/Scripts/WaypointScript.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaypointScript : MonoBehaviour {
+
+    public int queuePosition = 0;
+
+}
